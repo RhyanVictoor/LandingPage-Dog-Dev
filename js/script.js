cbab8777df22b76d078d9f49b9191a0646b00c8e
RhyanVictor <rhyansinho.04@gmail.com>
2024-07-24 17:00:05 -0400

    JavaScript

--- js/script.js
+++ js/script.js
@@ -16,11 +16,59 @@ menuOpener.addEventListener("click", () => {
 
 //testimonials
 let testimonials = [
-    { quote: '"frase de exemplo"', origin: "cbs.svg" },
-    { quote: '"frase de exemplo"', origin: "cbs.svg" },
-    { quote: '"frase de exemplo"', origin: "cbs.svg" },
-    { quote: '"frase de exemplo"', origin: "cbs.svg" },
+    {
+        quote: '"Mais do que nunca, os animais de estimação são tratados como membros da família."',
+        origin: "cbs.svg",
+    },
+    {
+        quote: '"Mais do que nunca, os animais de estimação são tratados como membros da família."',
+        origin: "forbes.svg",
+    },
+    {
+        quote: '"DogDev usa ingredientes simples e limpos em seus pratos"',
+        origin: "fox.svg",
+    },
+    {
+        quote: '"Vejo você [João] como um verdadeiro herói"',
+        origin: "sharktank.svg",
+    },
 ];
 
 let testimonialQuote = document.querySelector(".testimonials .quote");
 let testimonialIcons = document.querySelector(".testimonials .icons");
+
+for (let tindex in testimonials) {
+    let img = document.createElement("img");
+    img.setAttribute(
+        "src",
+        "./imagens/" + testimonials[parseInt(tindex)].origin
+    );
+    img.style.cursor = "pointer";
+    img.addEventListener("click", () => fillTestimonial(parseInt(tindex)));
+    testimonialIcons.appendChild(img);
+}
+
+let currentTestimonial = 0;
+let testimonialTimer;
+
+const fillTestimonial = (index) => {
+    clearTimeout(testimonialTimer);
+    currentTestimonial = index;
+    testimonialQuote.innerHTML = testimonials[currentTestimonial].quote;
+    let imgs = testimonialIcons.querySelectorAll("img");
+    for (let img of imgs) {
+        img.style.opacity = 0.2;
+    }
+    imgs[currentTestimonial].style.opacity = 1;
+    testimonialTimer = setTimeout(nextTestimonial, 3000);
+};
+
+const nextTestimonial = () => {
+    if (testimonials[currentTestimonial + 1]) {
+        fillTestimonial(currentTestimonial + 1);
+    } else {
+        fillTestimonial(0);
+    }
+};
+
+nextTestimonial();
